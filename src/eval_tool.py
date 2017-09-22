18945f9fb22ffd49989f3c16e54bad10f5df4610
Nithum Thain <nthain@nthain-macbookpro1.roam.corp.google.com>
2017-09-22 12:23:24 -0230

    responding to reviewer comments

--- src/eval_tool.py
+++ src/eval_tool.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import cPickle
 import os
 import pandas as pd
@@ -13,17 +17,15 @@ def compute_auc(y_true, y_pred):
     fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
     return metrics.auc(fpr, tpr)
 
-class EvalTool():
+class Model():
     def __init__(self, 
-                 model_version = None, 
+                 model_name, 
                  model_dir = '../models'):
         self.model_dir = model_dir
-        if model_version:
-            self.load_model_and_tokenizer(model_version)
-            
-    def load_model_and_tokenizer(self, model_version):
-        self.model = load_model(os.path.join(self.model_dir, '%s_model.h5' % model_version))
-        self.tokenizer = cPickle.load(open(os.path.join(self.model_dir, '%s_tokenizer.pkl' % model_version), 'rb'))
+        self.model = load_model(os.path.join(self.model_dir, '%s_model.h5' % model_name))
+        self.tokenizer = cPickle.load(open(os.path.join(self.model_dir, 
+                                                        '%s_tokenizer.pkl' % model_name), 
+                                           'rb'))
         
     def prep_data(self, data_path, text_column, label_column = None, max_sequence_length = 1000):
         """Loads the data from a csv.
@@ -31,7 +33,13 @@ class EvalTool():
         Args:
             data_path: path to a csv file of the data to evaluate on.
             text_column: column containing comment text in the csv.
-            label_column: column containing toxicity label in the csv. If set to None, it is assumed the data is unlabelled.
+            label_column: column containing toxicity label in the csv. 
+                          If set to None, it is assumed the data is unlabelled.
+        
+        Returns:
+            text_data: A tokenized and padded text sequence as a model input.
+            text_labels: A set of labels corresponding to the input text.
+                         Returns None if no label_column is set.
         """
         
         data = pd.read_csv(data_path)
@@ -41,15 +49,12 @@ class EvalTool():
         
         text_labels = None
         if label_column:
-            text_labels = to_categorical(data[label_column])
+            text_labels = to_categorical(data[label_column])[:,1]
             
         return text_data, text_labels
             
     def predict(self, data):
-        return self.model.predict(data)
-    
-    def score_auc(self, labels, preds):
-        return compute_auc(labels[:,1], preds[:,1])
+        return self.model.predict(data)[:,1]
     
     def prep_data_and_predict(self, data_path, text_column, label_column = None):
         data, labels = self.prep_data(data_path, text_column, label_column)
@@ -58,7 +63,7 @@ class EvalTool():
     def prep_data_and_score(self, data_path, text_column, label_column = None):
         data, labels = self.prep_data(data_path, text_column, label_column)
         preds = self.predict(data)
-        return self.score_auc(labels, preds)
+        return compute_auc(labels, preds)
         
         
     
