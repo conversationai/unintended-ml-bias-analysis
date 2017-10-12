742ec9823d8e54266ad5502b06dfe0e17bafa027
john li <jetpack@google.com>
2017-10-12 19:28:47 -0400

    model_tool clean up.
    Split out tokenizer fitting from data prep, simplified data prep, provide a
    simple 'predict' method for scoring text.

--- src/model_tool.py
+++ src/model_tool.py
@@ -7,8 +7,8 @@ print('HELLO from model_tool')
 import cPickle
 import json
 import os
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
@@ -46,7 +46,7 @@ DEFAULT_HPARAMS = {
 
 
 def compute_auc(y_true, y_pred):
-    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
+    fpr, tpr, _thresholds = metrics.roc_curve(y_true, y_pred)
     return metrics.auc(fpr, tpr)
 
 class ToxModel():
@@ -74,7 +74,7 @@ class ToxModel():
 
     def update_hparams(self, new_hparams):
         self.hparams.update(new_hparams)
-        
+
     def get_model_name(self):
         return self.model_name
 
@@ -93,54 +93,39 @@ class ToxModel():
                 '%s_hparams.json' % self.model_name), 'r') as f:
             self.hparams = json.load(f)
 
-    def prep_data(self, data_path, text_column, label_column = None, is_training = False):
-        """Loads the data from a csv.
-        
-        Args:
-            data_path: path to a csv file of the data to evaluate on.
-            text_column: column containing comment text in the csv.
-            label_column: column containing toxicity label in the csv. 
-                          If set to None, it is assumed the data is unlabelled.
-            is_training: Indicate if the data is training data on which to initialize
-                          the tokenizer.
-        
-        Returns:
-            text_data: A tokenized and padded text sequence as a model input.
-            text_labels: A set of labels corresponding to the input text.
-                         Returns None if no label_column is set.
-        """
-        
-        data = pd.read_csv(data_path)
-        text = data[text_column]
-        if is_training:
-            self.tokenizer = Tokenizer(num_words = self.hparams['max_num_words'])
-            self.tokenizer.fit_on_texts(text)
-            self.word_index = self.tokenizer.word_index
-            cPickle.dump(self.tokenizer, open(os.path.join(self.model_dir, '%s_tokenizer.pkl' % self.model_name), 'wb'))
-        text_sequences = self.tokenizer.texts_to_sequences(text)
+    def fit_and_save_tokenizer(self, texts):
+        """Fits tokenizer on texts and pickles the tokenizer state."""
+        self.tokenizer = Tokenizer(num_words = self.hparams['max_num_words'])
+        self.tokenizer.fit_on_texts(texts)
+        cPickle.dump(self.tokenizer, open(os.path.join(self.model_dir, '%s_tokenizer.pkl' % self.model_name), 'wb'))
 
-        text_data = pad_sequences(text_sequences, maxlen = self.hparams['max_sequence_length'])
-        
-        text_labels = None
-        if label_column:
-            text_labels = to_categorical(data[label_column])
-            
-        return text_data, text_labels
+    def prep_text(self, texts):
+        """Turns text into into padded sequences.
+
+        The tokenizer must be initialized before calling this method.
+
+        Args:
+            texts: Sequence of text strings.
+
+        Returns:
+            A tokenized and padded text sequence as a model input.
+        """
+        text_sequences = self.tokenizer.texts_to_sequences(texts)
+        return pad_sequences(text_sequences, maxlen=self.hparams['max_sequence_length'])
 
     def load_embeddings(self, embedding_path = DEFAULT_EMBEDDINGS_PATH):
-        self.embeddings_index = {}
-        f = open(embedding_path)
-        for line in f:
-            values = line.split()
-            word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
-            self.embeddings_index[word] = coefs
-        f.close()
+        embeddings_index = {}
+        with open(embedding_path) as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                embeddings_index[word] = coefs
 
-        self.embedding_matrix = np.zeros((len(self.word_index) + 1, self.hparams['embedding_dim']))
+        self.embedding_matrix = np.zeros((len(self.tokenizer.word_index) + 1, self.hparams['embedding_dim']))
         num_words_in_embedding = 0
-        for word, i in self.word_index.items():
-            embedding_vector = self.embeddings_index.get(word)
+        for word, i in self.tokenizer.word_index.items():
+            embedding_vector = embeddings_index.get(word)
             if embedding_vector is not None:
                 num_words_in_embedding += 1
                 # words not found in embedding index will be all-zeros.
@@ -150,9 +135,18 @@ class ToxModel():
         self.model_name = model_name
         self.save_hparams(model_name)
 
+        train_data = pd.read_csv(training_data_path)
+        valid_data = pd.read_csv(validation_data_path)
+
+        print('Fitting tokenizer...')
+        self.fit_and_save_tokenizer(train_data[text_column])
+        print('Tokenizer fitted!')
+
         print('Preparing data...')
-        train_data, train_labels = self.prep_data(training_data_path, text_column, label_column, is_training = True)
-        valid_data, valid_labels = self.prep_data(validation_data_path, text_column, label_column)
+        train_text, train_labels = (self.prep_text(train_data[text_column]),
+                                    to_categorical(train_data[label_column]))
+        valid_text, valid_labels = (self.prep_text(valid_data[text_column]),
+                                    to_categorical(valid_data[label_column]))
         print('Data prepared!')
 
         print('Loading embeddings...')
@@ -164,18 +158,19 @@ class ToxModel():
         print('Training model...')
 
         save_path = os.path.join(self.model_dir, '%s_model.h5' % self.model_name)
-        callbacks = [ModelCheckpoint(save_path, save_best_only = True, verbose=self.hparams['verbose'])]
+        callbacks = [ModelCheckpoint(save_path, save_best_only=True, verbose=self.hparams['verbose'])]
 
         if self.hparams['stop_early']:
-            callbacks.append(EarlyStopping(min_delta=self.hparams['es_min_delta'], 
+            callbacks.append(EarlyStopping(min_delta=self.hparams['es_min_delta'],
                 monitor='val_loss', patience=self.hparams['es_patience'], verbose=self.hparams['verbose'], mode='auto'))
-        
-        self.model.fit(train_data, train_labels,
-          batch_size=self.hparams['batch_size'],
-          epochs=self.hparams['epochs'],
-          validation_data=(valid_data, valid_labels),
-          callbacks=callbacks,
-          verbose = 2)
+
+        self.model.fit(train_text,
+                       train_labels,
+                       batch_size=self.hparams['batch_size'],
+                       epochs=self.hparams['epochs'],
+                       validation_data=(valid_text, valid_labels),
+                       callbacks=callbacks,
+                       verbose=2)
         print('Model trained!')
         print('Best model saved to {}'.format(save_path))
         print('Loading best model from checkpoint...')
@@ -184,7 +179,7 @@ class ToxModel():
 
     def build_model(self):
         sequence_input = Input(shape=(self.hparams['max_sequence_length'],), dtype='int32')
-        embedding_layer = Embedding(len(self.word_index) + 1,
+        embedding_layer = Embedding(len(self.tokenizer.word_index) + 1,
                                     self.hparams['embedding_dim'],
                                     weights=[self.embedding_matrix],
                                     input_length=self.hparams['max_sequence_length'],
@@ -194,7 +189,7 @@ class ToxModel():
         x = embedded_sequences
         for filter_size, kernel_size, pool_size in zip(self.hparams['cnn_filter_sizes'], self.hparams['cnn_kernel_sizes'], self.hparams['cnn_pooling_sizes']):
             x = self.build_conv_layer(x, filter_size, kernel_size, pool_size)
-            
+
         x = Flatten()(x)
         x = Dropout(self.hparams['dropout_rate'])(x)
         # TODO(nthain): Parametrize the number and size of fully connected layers
@@ -215,18 +210,15 @@ class ToxModel():
             # TODO(nthain): This seems broken. Fix.
             output = GlobalMaxPooling1D()(output)
         return output
-            
-    def predict(self, data):
+
+    def predict(self, texts):
+        """Returns model predictions on texts."""
+        data = self.prep_text(texts)
         return self.model.predict(data)[:,1]
-    
-    def prep_data_and_predict(self, data_path, text_column, label_column = None):
-        data, labels = self.prep_data(data_path, text_column, label_column)
-        return self.predict(data)
-    
-    def prep_data_and_score(self, data_path, text_column, label_column = None):
-        data, labels = self.prep_data(data_path, text_column, label_column)
-        preds = self.predict(data)
-        return compute_auc(labels[:,1], preds)
-        
+
+    def score_auc(self, texts, labels):
+        preds = self.predict(texts)
+        return compute_auc(labels, preds)
+
     def summary():
         return self.model.summary()
