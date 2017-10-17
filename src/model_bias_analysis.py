c35505a9b77642924b7c3c707dc0544929cea09f
john li <jetpack@google.com>
2017-10-17 19:57:12 -0400

    Move per-term AUC code to module.

--- src/model_bias_analysis.py
+++ src/model_bias_analysis.py
@@ -20,6 +20,7 @@ from model_tool import ToxModel, compute_auc
 MODEL_DIR = '../models/'
 ORIG_MADLIBS_PATH = '../eval_datasets/bias_madlibs_89k.csv'
 SCORED_MADLIBS_PATH = '../eval_datasets/bias_madlibs_89k_scored.csv'
+MADLIBS_TERMS_PATH = 'bias_madlibs_data/adjectives_people.txt'
 
 ### Model scoring
 
@@ -78,3 +79,49 @@ def plot_model_family_auc(dataset, model_names, label_col, min_auc=0.9):
     plt.gca().set_xlim([min_auc, 1.0])
     plt.show()
     return result
+
+## Per-term AUC analysis.
+
+def read_madlibs_terms():
+    with open(MADLIBS_TERMS_PATH) as f:
+        return [term.strip() for term in f.readlines()]
+
+def balanced_term_subset(df, term, text_col):
+    """Returns data subset containing term balanced with sample of other data.
+
+    We draw a random sample from the dataset of other examples because we don't
+    care about the model's ability to distinguish toxic from non-toxic just
+    within the term-specific dataset, but rather its ability to distinguish for
+    the term-specific subset within the context of a larger distribution of
+    data.
+    """
+    term_df = df[df[text_col].str.contains(r'\b{}\b'.format(term), case=False)]
+    nonterm_df = df[~df.index.isin(term_df.index)].sample(len(term_df))
+    combined = pd.concat([term_df, nonterm_df])
+    return combined
+
+def model_family_name(model_names):
+    """Given a list of model names, returns the common prefix."""
+    prefix = os.path.commonprefix(model_names)
+    if not prefix:
+        raise ValueError("couldn't determine family name from model names")
+    return prefix.strip('_')
+
+def per_term_aucs(dataset, terms, model_families, text_col, label_col):
+    """Computes per-term 'pinned' AUC scores for each model family."""
+    records = []
+    for term in terms:
+        term_subset = balanced_term_subset(dataset, term, text_col)
+        term_record = {'term': term, 'subset_size': len(term_subset)}
+        for model_family in model_families:
+            family_name = model_family_name(model_family)
+            aucs = [compute_auc(term_subset[label_col], term_subset[model_name])
+                    for model_name in model_family]
+            term_record.update({
+                family_name + '_mean': np.mean(aucs),
+                family_name + '_median': np.median(aucs),
+                family_name + '_std': np.std(aucs),
+                family_name + '_aucs': aucs,
+            })
+        records.append(term_record)
+    return pd.DataFrame(records)
