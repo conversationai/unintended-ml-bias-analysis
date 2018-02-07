e21621af6d8be09a76decdb069d3927df940de54
JB <j@rubinovitz.com>
2018-02-06 19:39:01 -0500

    latest

--- src/model_tool.py
+++ src/model_tool.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import cPickle
+import datetime
 import json
 import os
 from keras.callbacks import EarlyStopping
