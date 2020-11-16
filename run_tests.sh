97c5603fa19a851c90b834c5a9a2107eeeb61fa5
Jeffrey Sorensen <sorenj@google.com>
2020-11-16 13:01:47 -0500

    Remove pandoc, switch to notebook test output.

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,5 @@ python model_bias_analysis_test.py
 popd
 
 # IPython "Tests" execute notebooks
-jupyter-nbconvert --to asciidoc --execute \
+jupyter-nbconvert --to notebook --execute \
   presentations/FAT_Star_Tutorial_Measuring_Unintended_Bias_in_Text_Classification_Models_with_Real_Data.ipynb
